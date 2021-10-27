9916798a8e2822b9b02fac8ac33879548923d466
Prince Kumar <37762770+Prince-1501@users.noreply.github.com>
2021-10-27 10:50:47 +0530

    customer Hacktoberfest add verifyHacktober

---
+++ routes/customer-hacktoberfest.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const multer = require("multer");
+
+const router = express.Router();
+
+const {
+  createCustomerRequest,
+  fileUpload,
+} = require("../controllers/customer");
+
+const { verifyToken } = require("../Middlewares/verifyAuth");
+const { verifyCountry } = require("../Middlewares/verifyCountryAndDB");
+const { mongoConnection } = require("../database/mongo");
+
+// Multer ships with storage engines DiskStorage and MemoryStorage
+// And Multer adds a body object and a file or files object to the request object. The body object contains the values of the text fields of the form, the file or files object contains the files uploaded via the form.
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+router.post(
+  verifyHacktober,
+  verifyHacktoberCountry,
+  mongoConnection
+);
+
+module.exports = router;
